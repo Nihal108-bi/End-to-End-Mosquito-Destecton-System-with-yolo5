765c59470982c2eb975b339468a10a809eb5bd51
Nihal Jaiswal <nihaljaisawal1@gmail.com>
2026-04-06 21:04:13 +0530

    setup.py adding

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name="mosquito-detector",
+    version="0.1.0",
+    author="Nihal",
+    author_email="nihaljaisawal1@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
